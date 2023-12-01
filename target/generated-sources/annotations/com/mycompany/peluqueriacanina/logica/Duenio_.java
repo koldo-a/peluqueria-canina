81865a81c504116a7b197a1006a82b74289768e0
koldoarretxea <koldo.arretxea@gmail.com>
2023-12-01 10:26:24 +0100

    First commit

---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Duenio_.java
@@ -0,0 +1,16 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-30T19:33:09", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Duenio.class)
+public class Duenio_ { 
+
+    public static volatile SingularAttribute<Duenio, String> direccion;
+    public static volatile SingularAttribute<Duenio, String> celular;
+    public static volatile SingularAttribute<Duenio, String> nombre;
+    public static volatile SingularAttribute<Duenio, Integer> id_duenio;
+
+}
---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Mascota_.java
@@ -0,0 +1,21 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import com.mycompany.peluqueriacanina.logica.Duenio;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-30T19:33:09", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, Integer> num_cliente;
+    public static volatile SingularAttribute<Mascota, String> nombre_perro;
+    public static volatile SingularAttribute<Mascota, String> raza;
+    public static volatile SingularAttribute<Mascota, String> color;
+    public static volatile SingularAttribute<Mascota, String> alergico;
+    public static volatile SingularAttribute<Mascota, String> observaciones;
+    public static volatile SingularAttribute<Mascota, Duenio> unDuenio;
+    public static volatile SingularAttribute<Mascota, String> atencion_especial;
+
+}
